dd21a064924a9c72c7ed26b03a4697209daf4466
E-Liang Tan <eliang@eliangtan.com>
2017-02-01 10:29:33 +0800

    A2: Completed Assignment 2.

--- assignments/assignment2/assignment2-solution/js/app.js
+++ assignments/assignment2/assignment2-solution/js/app.js
@@ -1,6 +1,47 @@
 (function() {
 "use strict";
 
-angular.module("ShoppingListCheckOff", []);
+angular.module("ShoppingListCheckOff", [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+    this.items = ShoppingListCheckOffService.getItemsToBuy();
+
+    this.boughtItemAtIndex = function(index) {
+        ShoppingListCheckOffService.boughtItemAtIndex(index);
+    }
+}
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+    this.items = ShoppingListCheckOffService.getItemsAlreadyBought();
+}
+
+ShoppingListCheckOffService.$inject = [];
+function ShoppingListCheckOffService() {
+    this.toBuy = [ { name: "cookies", quantity: 10 },
+                    { name: "cans of coffee", quantity: 20 },
+                    { name: "cartons of milk", quantity: 3 },
+                    { name: "apples", quantity: 365 },
+                    { name: "bottle of olive oil", quantity: 1 } ];
+    this.alreadyBought = [];
+
+    this.getItemsToBuy = function() {
+        return this.toBuy;
+    }
+
+    this.getItemsAlreadyBought = function() {
+        return this.alreadyBought;
+    }
+
+    this.boughtItemAtIndex = function(index) {
+        var item = this.toBuy[index];
+        this.toBuy.splice(index, 1);
+        this.alreadyBought.push(item);
+    }
+}
 
 })();
